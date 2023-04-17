ec72e5276fd8e4082150b51239764fc626b462cf
michal-sekulski <michal.sekulski@skyscanner.net>
2023-04-17 22:01:47 +0100

    Working state but ranking logic is wrong

--- EurovisionRanking/ContentView.swift
+++ EurovisionRanking/ContentView.swift
@@ -14,39 +14,62 @@ class SongsHelper {
     static func generateTwoSongs() -> (Song, Song) {
         let firstSong: Song
         let secondSong: Song
-        
-        //if rankedSongs.count > 0 {
-            //firstSong
-            
-            //} else {
+
+        let repeatedScores = findSongsWithSameScore()
+         if rankedSongs.count == 0 || repeatedScores == nil {
+            // TODO: Add a case when went through all the songs
             let firstIndex = Int.random(in: 0..<songs.count)
             firstSong = songs.remove(at: firstIndex)
             let secondIndex = Int.random(in: 0..<songs.count)
             secondSong = songs.remove(at: secondIndex)
-        //}
+        } else {
+            firstSong = repeatedScores!.0
+            secondSong = repeatedScores!.1
+        }
         return (firstSong, secondSong)
     }
     
-    static func assignScores(winner: Song, loser: Song) {
-        print("winner: \(winner.country)")
+    static func addRankedSongs(songs: [Song]) {
+        songs.forEach { song in
+            if let index = rankedSongs.firstIndex(where: { $0.videoID == song.videoID }) {
+                rankedSongs[index].score += song.score
+            } else {
+                let index = rankedSongs.firstIndex(where: { $0.score >= song.score }) ?? rankedSongs.endIndex
+                rankedSongs.insert(song, at: index)
+            }
+        }
+        print(rankedSongs)
+    }
+    
+    static func findSongsWithSameScore() -> (Song, Song)? {
+        var scores: Set<Int> = []
+        for song in rankedSongs {
+            if scores.contains(song.score) {
+                let previousSong = rankedSongs.first(where: { $0.score == song.score })!
+                return (previousSong, song)
+            } else {
+                scores.insert(song.score)
+            }
+        }
+        return nil
     }
 }
 
 struct ContentView: View {
     @State private var song1: Song = Song(country: "", videoID: "", score: 0)
     @State private var song2: Song = Song(country: "", videoID: "", score: 0)
-    
-    let sdm = SongDataManager()
+    @State private var finishedRanking = false
+    //let sdm = SongDataManager()
     
     var body: some View {
         VStack {
-//            Image("EUROVISION")
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 100, height: 100)
             SongView(song: song1)
             Button {
-                SongsHelper.assignScores(winner: song1, loser: song2)
+                song1.score += 1
+                song2.score -= 1
+                SongsHelper.addRankedSongs(songs: [song1, song2])
+                (song1, song2) = SongsHelper.generateTwoSongs()
+                //SongsHelper.setScores(winner: song1, loser: song2)
                 //sdm.modify(string: "Clicked \(song1.country)")
             } label: {
                 Text("Choose")
@@ -57,15 +80,25 @@ struct ContentView: View {
             
             SongView(song: song2)
             Button {
-                sdm.modify(string: "Clicked \(song2.country)")
+                song1.score -= 1
+                song2.score += 1
+                SongsHelper.addRankedSongs(songs: [song1, song2])
+                (song1, song2) = SongsHelper.generateTwoSongs()
             } label: {
                 Text("Choose")
             }.buttonStyle(.bordered)
         }.padding()
+            .sheet(isPresented: $finishedRanking) {
+                SecondView() // <- pass rankedListOfSongs: <rankedSongs>
+            }
             .onAppear {
-                let selectedSongs = SongsHelper.generateTwoSongs()
-                song1 = selectedSongs.0
-                song2 = selectedSongs.1
+                if SongsHelper.songs.count == 0 {
+                    finishedRanking = true
+                } else {
+                    let selectedSongs = SongsHelper.generateTwoSongs()
+                    song1 = selectedSongs.0
+                    song2 = selectedSongs.1
+                }
             }
     }
 }
@@ -75,3 +108,45 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct SecondView: View {
+    //let rankedListOfSongs: [Song]
+    var body: some View {
+        VStack {
+            Image("EUROVISION")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            Text("Sheet") // <- pass rankedListOfSongs
+        }
+    }
+}
+
+//class SongsDataManager: ObservedObject {
+//    @Published var song1: Song
+//    @Published var song2: Song
+//
+//    var allSongs = Song.data
+//
+//    init(song1: Song, song2: Song) {
+//        let (song1, song2) = generateUniqueSongs()
+//        self.song1 = song1
+//        self.song2 = song2
+//    }
+//
+//    public func generateUniqueSongs() -> (Song, Song) {
+//        let firstSong: Song
+//        let secondSong: Song
+//
+//        //if rankedSongs.count > 0 {
+//            //firstSong
+//
+//            //} else {
+//            let firstIndex = Int.random(in: 0..<allSongs.count)
+//            firstSong = allSongs.remove(at: firstIndex)
+//            let secondIndex = Int.random(in: 0..<allSongs.count)
+//            secondSong = allSongs.remove(at: secondIndex)
+//        //}
+//        return (firstSong, secondSong)
+//    }
+//}

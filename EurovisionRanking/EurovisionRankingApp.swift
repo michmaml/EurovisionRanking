d8e21d1aa95004e3ae6615acf687684f23a977e4
michal-sekulski <michal.sekulski@skyscanner.net>
2023-04-17 11:43:06 +0100

    Initial Commit

---
+++ EurovisionRanking/EurovisionRankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EurovisionRankingApp.swift
+//  EurovisionRanking
+//
+//  Created by Michal Sekulski on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EurovisionRankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

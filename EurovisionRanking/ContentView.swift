600a2a7b282ed52d9b156bd53d3cc174fbb338de
michal-sekulski <michal.sekulski@skyscanner.net>
2023-06-21 11:40:46 +0100

    Adds a view model, logic WIP

--- EurovisionRanking/ContentView.swift
+++ EurovisionRanking/ContentView.swift
@@ -9,124 +9,37 @@ import SwiftUI
 import Backpack_SwiftUI
 
 struct ContentView: View {
-    let network = Network()
-    @State var baseSongs = [Network.BaseSong]()
-    @State var songs = [Song]()
-    @State var rankedSongs: [Song] = []
-    @State var comparedPairs: Set<String> = []
-    @State private var song1: Song = Song(country: "", artist: "", title: "", videoID: "", wins: 0, losses: 0)
-    @State private var song2: Song = Song(country: "", artist: "", title: "", videoID: "", wins: 0, losses: 0)
-    @State private var finishedRanking = false
-    let svm: SongViewModel
-    
+    @StateObject private var viewModel = ViewModel()
     var body: some View {
         ZStack {
             Color(BPKColor.canvasColor).ignoresSafeArea()
             VStack {
-                BPKCard {
-                    VStack {
-                        if songs.count != 0 {
-                            SongView(song: song1)
-                            BPKButton("Choose", action: {
-                                handleButtonBehaviour(forWinnerSong: &song1, loserSong: &song2)
-                            })
-                        } else {
-                            CardLoadingView()
+                ForEach(viewModel.activeSongs) { song in
+                    BPKCard {
+                        VStack {
+                            SongView(song: song)
                         }
-                    }
-                }
-
-                Spacer()
-                Spacer()
-
-                BPKCard {
-                    VStack {
-                        if songs.count != 0 {
-                            SongView(song: song2)
-                            BPKButton("Choose", action: {
-                                handleButtonBehaviour(forWinnerSong: &song2, loserSong: &song1)
-                            })
-                        } else {
-                            CardLoadingView()
-                        }
+                    }.onTapGesture {
+                        viewModel.selectWinner(for: song)
                     }
                 }
             }
             .padding()
-            .sheet(isPresented: $finishedRanking) {
-                SummaryView(songs: rankedSongs)
+            .sheet(isPresented: $viewModel.finishedRanking) {
+                //SummaryView(songs: rankedSongs)
             }
-        }
-        .task {
-            do {
-                baseSongs = try await network.getAllSongs()
-                mapBaseSongsToSongs()
-                if songs.count == 0 {
-                    finishedRanking = true
-                } else {
-                    let selectedSongs = generateTwoSongs()
-                    song1 = selectedSongs.0
-                    song2 = selectedSongs.1
-                }
-            } catch {
-                print("Error", error)
+            .onAppear { // <- change to onload
+                // load data from api, viewModel.onAppStart()
+                // ^ this will trigger the loading state
+                // ^^ add loading screen
+                viewModel.startRanking()
             }
-        }
-    }
-    
-    func mapBaseSongsToSongs() {
-        songs = baseSongs.map {
-            Song(country: $0.countryName, artist: $0.artistName, title: $0.songName, videoID: $0.videoId, wins: 0, losses: 0)
-        }
-    }
-    
-    func generateTwoSongs() -> (Song, Song) {
-        var firstSong: Song
-        var secondSong: Song
-
-        let repeatedScores = svm.findSongsWithSameScore(forRankedSongs: rankedSongs, comparedPairs: comparedPairs)
-        if rankedSongs.count == 0 || repeatedScores == nil {
-           let firstIndex = Int.random(in: 0..<songs.count)
-           firstSong = songs.remove(at: firstIndex)
-           let secondIndex = Int.random(in: 0..<songs.count)
-           secondSong = songs.remove(at: secondIndex)
-        } else {
-           firstSong = repeatedScores!.0
-           secondSong = repeatedScores!.1
-        }
-        
-        return (firstSong, secondSong)
-    }
-    
-    func addRankedSongs(songs: [Song]) {
-        songs.forEach { song in
-            if let index = rankedSongs.firstIndex(where: { $0.videoID == song.videoID }) {
-                rankedSongs[index] = song
-            } else {
-                let index = rankedSongs.firstIndex(where: {
-                    $0.wins + $0.losses >= song.wins + song.losses
-                }) ?? rankedSongs.endIndex
-                rankedSongs.insert(song, at: index)
-            }
-        }
-        comparedPairs.insert(svm.getComparisonKey(forPreviousSong: songs[0], currentSong: songs[1]))
-    }
-    
-    func handleButtonBehaviour(
-        forWinnerSong winnerSong: inout Song, loserSong: inout Song) {
-        winnerSong.wins += 1
-        loserSong.losses += 1
-        addRankedSongs(songs: [song1, song2])
-        if songs.count == 0 {
-            finishedRanking = true
-        } else {
-            (song1, song2) = generateTwoSongs()
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(svm: SongViewModel())
+        ContentView()
     }
 }
---
+++ EurovisionRanking/ViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ViewModel.swift
+//  EurovisionRanking
+//
+//  Created by Michal Sekulski on 21/06/2023.
+//
+
+import SwiftUI
+
+@MainActor class ViewModel: ObservableObject {
+    @Published private(set) var activeSongs = [Song]()
+    @Published var finishedRanking = false
+    
+    private let maxActiveSongs = 2
+    private var allSongs = Song.data
+    
+    private func generateActiveSongs() -> [Song] {
+        return [allSongs.randomElement()!, allSongs.randomElement()!]
+    }
+    
+    func startRanking() {
+        activeSongs = generateActiveSongs()
+    }
+    
+    func selectWinner(for winnerSong: Song) {
+        // compare logic
+        activeSongs = generateActiveSongs()
+    }
+}
